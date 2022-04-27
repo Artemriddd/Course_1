31b7854974c4eb036e3f9b4107e0d11ba439113b
Artem <Artemrid@yandex.ru>
2022-04-27 21:30:49 +0300

    Homework_1

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string strFio;
+            strFio = Console.ReadLine();
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("Привет, {0}, сегодня {1}",strFio,dt);
+            Console.ReadKey();
+        }
+    }
+}
